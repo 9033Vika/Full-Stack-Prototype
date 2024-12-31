1b5d0bff8fe87f211f1d81a3aa6b308cd5e4603a
Vikas <106235260+vikaswebdeveloper@users.noreply.github.com>
2024-12-31 16:02:42 +0530

    Update cart.js

--- server/controller/cart.js
+++ server/controller/cart.js
@@ -351,7 +351,7 @@ export const verify = async (req, res) => {
 
     await user.save();
 
-    res.redirect(`http://course-selling-full-stack-prototype-frontend.vercel.app/paymentSuccess/${razorpay_payment_id}`);
+    res.redirect(`https://course-selling-full-stack-prototype-frontend.vercel.app/paymentSuccess/${razorpay_payment_id}`);
   } catch (error) {
     return res.status(500).json({
       success: false,
